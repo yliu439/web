414b1f58c62a5305d82ad494d62ebf8eabaae39c
yliu439 <12885039@qq.com>
2018-09-11 22:21:01 +0800

    v1

--- src/main/java/com/liuyang/web/demo/DemoController.java
+++ src/main/java/com/liuyang/web/demo/DemoController.java
@@ -1,4 +1,22 @@
 package com.liuyang.web.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping(value="/index")
+    public String index(){
+        return "Hello LiuYang!";
+    }
+
+    @GetMapping(value="/admin")
+    public String admin(){
+        return "Hello Admin!";
+    }
 }
